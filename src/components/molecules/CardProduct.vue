d153a157b835da5e70134ac2708ead9efb5a6ad4
rizanoor <rizanoorfauzan123@gmail.com>
2024-05-27 15:40:04 +0700

    feat: create shopView

---
+++ src/components/molecules/CardProduct.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+    <div class="col-12 col-md-4 col-lg-3 mb-5">
+        <a class="product-item" href="#">
+            <img src="../../assets/images/product-3.png" class="img-fluid product-thumbnail">
+            <h3 class="product-title">Nordic Chair</h3>
+            <strong class="product-price">$50.00</strong>
+
+            <span class="icon-cross">
+                <img src="../../assets/images/cross.svg" class="img-fluid">
+            </span>
+        </a>
+    </div>
+</template>
---
+++ src/views/ShopView.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CardProduct from '../components/molecules/CardProduct.vue';
+import Hero from '../components/organisems/Hero.vue';
+
+</script>
+
+<template>
+    <main>
+        <Hero title="Shop"
+            description="Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique." />
+
+        <div class="untree_co-section product-section before-footer-section">
+            <div class="container">
+                <div class="row">
+                    <CardProduct v-for="n in 8" :key="n" />
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
