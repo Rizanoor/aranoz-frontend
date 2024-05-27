325137eb6f7b610e3d0974306cbaffbfcc0e3e0d
rizanoor <rizanoorfauzan123@gmail.com>
2024-05-27 08:32:53 +0700

    feat: slicing home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,20 @@
 <script setup>
+import Blog from '../components/organisems/Blog.vue';
+import Choose from '../components/organisems/Choose.vue';
+import Help from '../components/organisems/Help.vue';
+import Hero from '../components/organisems/Hero.vue';
+import Popular from '../components/organisems/Popular.vue';
+import Product from '../components/organisems/Product.vue';
+
 </script>
 
 <template>
   <main>
-  
+    <Hero />
+    <Product />
+    <Choose />
+    <Help />
+    <Popular />
+    <Blog />
   </main>
 </template>
