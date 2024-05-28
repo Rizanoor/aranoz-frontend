e1169f41859f10f8fc0ac64903f671cfdeee8805
rizanoor <rizanoorfauzan123@gmail.com>
2024-05-28 14:59:03 +0700

    feat: integrasi login and dynamic navbar

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -1,11 +1,33 @@
 <script setup>
-import { RouterLink, useRoute } from 'vue-router';
+import { ref, onMounted } from 'vue';
+import { RouterLink, useRouter } from 'vue-router';
+
+const userName = ref('');
+const isLoggedIn = ref(false);
+const router = useRouter();
+
+onMounted(() => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+        isLoggedIn.value = true;
+        userName.value = localStorage.getItem('userName');
+    }
+});
+
+const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('userName');
+    localStorage.removeItem('userEmail');
+    isLoggedIn.value = false;
+    userName.value = '';
+    router.push('/login');
+};
 </script>
 
 <template>
     <nav class="custom-navbar navbar navbar-expand-md navbar-dark bg-dark" aria-label="Furni navigation bar">
         <div class="container">
-            <a class="navbar-brand" href="/">Furni<span>.</span></a>
+            <RouterLink class="navbar-brand" to="/">Furni<span>.</span></RouterLink>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni"
                 aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -39,7 +61,19 @@ import { RouterLink, useRoute } from 'vue-router';
                     </li> -->
                 </ul>
                 <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-                    <li><a class="nav-link" href="#"><img src="../../assets/images/user.svg" alt="User"></a></li>
+                    <li v-if="!isLoggedIn">
+                        <RouterLink class="nav-link" to="/login">
+                            <img src="../../assets/images/user.svg" alt="User">
+                        </RouterLink>
+                    </li>
+                    <li v-else class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Hello, {{ userName }}
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" @click="handleLogout" href="#">Logout</a></li>
+                        </ul>
+                    </li>
                     <li>
                         <RouterLink to="/cart" class="nav-link">
                             <img src="../../assets/images/cart.svg" alt="Cart">
